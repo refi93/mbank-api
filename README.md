ab66c260ed400521d6b873a5b36c614e1a33783b
refi93 <rafael.korbas@gmail.com>
2018-11-19 23:00:21 +0100

    get account info by iban

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # mbank-api
+
+NodeJs library to interact with mBank internet banking api. Very basic functionality.
+
+Example:
+
+```
+const Mbank = require('mbank-api)
+
+const mbankSession = new Mbank('sk', 'johndoe', 'superstrongpwd')
+await mbankSession.login()
+
+console.log(mbankSession.getAccountByIban('myiban'))
+/*
+outputs something like this: 
+{ cID: 'SK8483600556293256363563',
+  cProductName: 'mKonto',
+  cSubTitle: '',
+  cAccountNumberForDisp: 'SK84 8360 0556 2932 5636 3563',
+  mBalance: 218234.32,
+  mAvailableBalance: 218231.93,
+  mOwnBalance: 218231.93,
+  cCurrency: 'EUR',
+  isAuxiliary: false,
+  actions:
+   [ { url: 'GetTransferOwn', name: 'transfer.own' },
+     { url: 'GetTransferDomestic', name: 'transfer.domestic' },
+     { url: 'GetAccountHistory', name: 'history' } ],
+  cProductCode: '42',
+  cSubproductCode: '0202' }
+*/
+```
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const axios = require('axios')
+const _ = require('lodash')
+
+class Mbank {
+  constructor(countryCode, username, password) {
+    this.username = username
+    this.password = password
+    this.baseUrl = `https://online.mbank.${countryCode}/${countryCode}`
+    this.cookie = undefined
+  }
+
+  async login() {
+    const url = `${this.baseUrl}/LoginMain/Account/JsonLogin`
+
+    const result = await axios.post(url, {
+      Username: this.username,
+      Password: this.password
+    })
+
+    this.cookie = result.headers['set-cookie']
+
+    return true
+  }
+
+  keepAlive() {
+    const url = `${this.baseUrl}/LoginMain/Account/JsonSessionKeepAlive`
+
+    return axios.post(url, {}, {
+      headers: {
+        Cookie: this.cookie
+      }
+    })
+  }
+
+  async getAccounts() {
+    const url = `${this.baseUrl}/Accounts/Accounts/List`
+
+    const response = await axios.post(url, {}, {
+      headers: {
+        Cookie: this.cookie
+      }
+    })
+
+    return response.data.properties.CurrentAccountsList
+  }
+
+  async getAccountByIban(iban) {
+    const accounts = await this.getAccounts()
+
+    return _.find(accounts, ['cID', iban])
+  }
+}
+
+module.exports = Mbank
